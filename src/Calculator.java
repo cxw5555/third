c0bba0e827a91e199405a5e9918de10bfd263d7f
18758116243 <18758116243@163.com>
2016-05-09 18:58:11 +0800

    ewrwr

---
+++ src/Calculator.java
@@ -0,0 +1,42 @@
+public class Calculator {
+    private static int result;    //
+    public void add(int n)
+    {
+        result=result+n;
+    }
+    
+    public void substract(int n)
+    {
+        result=result-n;        //Bug
+    }
+    
+    public void multiply(int n)
+    {
+    	result=result*n;//
+    }
+    
+    public void divide(int n)
+    {
+        result=result/n;
+    }
+    
+    public void square(int n)
+    {
+        result=n*n;
+    }
+    
+    public void squareRoot(int n)
+    {
+        for(;;);    //
+    }
+    
+    public void clear()
+    {
+        result=0;  //
+    }
+    
+    public int getResult()
+    {
+        return result;
+    }
+}
